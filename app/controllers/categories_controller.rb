bc6d5b5dfd6d5a7e3c999244c03d610867927e75
DadaLV <vynohradova.l.a@gmail.com>
2023-02-20 01:45:01 +0200

    add show category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+class CategoriesController < ApplicationController
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
+  resources :operations, only: [:show, :index]
+  resources :categories, only: [:show, :index]
 
 end
