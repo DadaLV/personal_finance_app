eac9cb79f9c39e77cabe322e85372c0dec947da4
DadaLV <vynohradova.l.a@gmail.com>
2023-02-26 21:10:59 +0200

    add user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_19_210546) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_174226) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_19_210546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_operations_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "operations", "categories"
