068f3650b744913032f3c88fdf0b5cfc2bea360a
DadaLV <vynohradova.l.a@gmail.com>
2023-02-26 14:47:31 +0200

    modify graphs in statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,15 +2,15 @@ class StatisticsController < ApplicationController
   include StatisticsHelper
   
   def index
-    @data_for_chart = []
-    operations_data = Operation.all.map { |op| [op.odate.strftime("%B %d, %Y"), op.description, op.amount.to_s, op.category.name ] }
-    operations_data_sorted = operations_data.sort
+    # @data_for_chart = []
+    # operations_data = Operation.all.map { |op| [op.odate.strftime("%B %d, %Y"), op.description, op.amount.to_s, op.category.name ] }
+    # operations_data_sorted = operations_data.sort
 
-    @dates = operations_data_sorted.map { |e| e[0] }
-    @amount = operations_data_sorted.map { |e| e[2].to_i }
-    @data_for_chart += operations_data
-    @operation_name = operations_data_sorted.map { |e| e[1] }
-    @operation_category = operations_data_sorted.map { |e| e[3] } 
+    # @dates = operations_data_sorted.map { |e| e[0] }
+    # @amount = operations_data_sorted.map { |e| e[2].to_i }
+    # @data_for_chart += operations_data
+    # @operation_name = operations_data_sorted.map { |e| e[1] }
+    # @operation_category = operations_data_sorted.map { |e| e[3] } 
   
   end
 
@@ -35,7 +35,8 @@ end
     operations_data = operations.map { |op| [op.odate.strftime("%B %d, %Y"), op.description, op.amount.to_s, op.category.name ] }
     operations_data_sorted = operations_data.sort
 
-    @dates = operations_data_sorted.map { |e| e[0] }
+    @datess = operations_data_sorted.map { |e| e[0] }
+    
     @amount = operations_data_sorted.map { |e| e[2].to_i }
     @operation_name = operations_data_sorted.map { |e| e[1] }
     @operation_category = operations_data_sorted.map { |e| e[3] }
