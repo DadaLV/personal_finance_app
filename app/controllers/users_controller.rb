9064d92c465c1f6353544bdf3d43375580d2af3b
DadaLV <vynohradova.l.a@gmail.com>
2023-02-27 02:52:01 +0200

    add edit in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated."
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome #{@user.username}! You have seccessfully sign up."
+      flash[:notice] = "Welcome #{@user.username}! You have successfully sign up."
       redirect_to root_path
     else
       render :new
