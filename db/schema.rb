55e031af8e4e5da66013a83c4afcf4fa073ec1d2
DadaLV <vynohradova.l.a@gmail.com>
2023-03-02 05:56:08 +0200

    modify dev and test config

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_26_222852) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_26_222852) do
     t.decimal "amount"
     t.datetime "odate"
     t.string "description"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
