e321b4405bba4fd81b31cef270d84868fdfbec69
DadaLV <vynohradova.l.a@gmail.com>
2023-03-02 13:44:27 +0200

    add category mode tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,6 +1,28 @@
 require 'test_helper'
 
-class CategoryTest <ActiveSupport::TestCase
+class CategoryTest < ActiveSupport::TestCase
   
-  
+  def setup  
+    @category = Category.new(name: "Food", description: "Food in shops")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Food", description: "Food in shops")
+    assert_not @category2.valid?
+  end
+
+  test "description should be present" do
+    @category.description = " "
+    assert_not @category.valid?
+  end
 end
