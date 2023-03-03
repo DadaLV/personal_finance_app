76d74ca10b594fdc4c928eab041b1d58f382c0fe
DadaLV <vynohradova.l.a@gmail.com>
2023-03-03 05:11:59 +0200

    modify statistics for postgresql

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,84 +1,34 @@
 class StatisticsController < ApplicationController
-  include StatisticsHelper
-  before_action :require_user
-  
-  def index
-    # @data_for_chart = []
-    # operations_data = Operation.all.map { |op| [op.odate.strftime("%B %d, %Y"), op.description, op.amount.to_s, op.category.name ] }
-    # operations_data_sorted = operations_data.sort
-
-    # @dates = operations_data_sorted.map { |e| e[0] }
-    # @amount = operations_data_sorted.map { |e| e[2].to_i }
-    # @data_for_chart += operations_data
-    # @operation_name = operations_data_sorted.map { |e| e[1] }
-    # @operation_category = operations_data_sorted.map { |e| e[3] } 
-  
-  end
-
-  
-def stati
-  
-  if params[:my_category].present?
-    category = Category.find_by(id: params[:my_category])
-    operations = category.present? ? category.operations : Operation.all
-    operations = current_user.operations
-  else  
-  operations = Operation.all
-  operations = current_user.operations
-end
-  
-  if params[:start_date].present? && params[:end_date].present?
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date]) +1.day
-      operations = operations.where(odate: start_date..end_date)
-    
-  end
-  
-
-    operations_data = operations.map { |op| [op.odate.strftime("%B %d, %Y"), op.description, op.amount.to_s, op.category.name ] }
-    operations_data_sorted = operations_data.sort
-
-    @datess = operations_data_sorted.map { |e| e[0] }
-    
-    @amount = operations_data_sorted.map { |e| e[2].to_i }
-    @operation_name = operations_data_sorted.map { |e| e[1] }
-    @operation_category = operations_data_sorted.map { |e| e[3] }
-
-end
-
-  def generate_month_options(from, to)
-    (from..to).map { |date| [date.strftime("%B %Y"), date.strftime("%Y-%m")] }.uniq
-  end
-
-  def doughnut_chart_data(month, category_id = nil)
-    @categories = Category.all
-    data = []
-
-    if month.is_a?(Range)
-      month_range = month
-    else
-      month_range = month.beginning_of_month..month.end_of_month
+    before_action :require_user
+    def index
     end
+  
+    def select_by_category
+      start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : nil
+      end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : nil
 
-    user_id = current_user.id
-
-    @categories.each do |category|
-      amount = Operation.where(user_id: user_id || current_user.id, category_id: category_id || category.id, odate: month_range).sum(:amount)
-      data << { label: category.name, value: amount }
+      operations = current_user.operations
+      operations = operations.where(odate: start_date..end_date) if start_date && end_date
+      @data = operations.data_for_reports(start_date, end_date).group("category_id").sum(:amount)
+  
+      operations_data = @data.transform_keys { |category| Category.find_by(id: category).name }.to_a
+  
+      @operations_name = operations_data.map { |e| e[0] }
+      @operations_amount = operations_data.map { |e| e[1] }
+      @operations = operations.order(odate: :desc)
     end
-
-    return data
-  end
-
-  def select_by_category
-    @selected_month = params[:month] ? Date.strptime(params[:month], '%Y-%m') : Date.today.beginning_of_month
-    @data_for_chart = doughnut_chart_data(@selected_month.beginning_of_month..@selected_month.end_of_month, current_user.id)
-    @months = generate_month_options(Date.today.beginning_of_year, Date.today.end_of_month)
+  
+    def stati
+      start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : nil
+      end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : nil
     
-  end
-
-  private
-
-  # def set_user_operstions
-  #   operation
+      user_operations = current_user.operations
+      @current_user_operations = user_operations.where(odate: start_date..end_date) if start_date && end_date
+      operations_data = user_operations.data_for_reports(start_date, end_date).group("odate").sum(:amount)
+    
+      @operations_date = operations_data.map { |key, value| key.strftime("%B %d, %Y") }
+      @operations_sum = operations_data.map { |key, value| value }
+    end
+    
+  
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -6,4 +6,9 @@ class Operation < ApplicationRecord
   validates :odate, presence: true
   validates :description, presence: true, length: { minimum: 3, maximum: 100 }
 
+  scope :data_for_reports, -> (start_date, end_date) {
+    operations = where(odate: start_date..end_date)
+    operations
+  }
+
 end
