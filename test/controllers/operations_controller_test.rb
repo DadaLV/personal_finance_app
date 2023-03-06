b3211e4b2291a0d25fe511afe8d82bd5cd794f94
DadaLV <vynohradova.l.a@gmail.com>
2023-03-07 00:49:17 +0100

    modify operations controller tests

--- test/controllers/operations_controller_test.rb
+++ test/controllers/operations_controller_test.rb
@@ -2,10 +2,12 @@ require "test_helper"
 
 class OperationsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @operation = Operation.create(amount: 25, odate: "Wed, 25 Jan 2023 00:00:00.000000000 UTC +00:00", description: "Pancake", category_id: 1, user_id: 1)
+    
+    @user = User.create(username: "johndoe1", email: "johndoe1@example.com", password: "password1")
     @user1 = User.create(username: "johndoe", email: "johndoe@example.com", password: "password")
     @category = Category.create(name: "Sports", description: "Sports")
     sign_in_as(@user1)
+    @operation = Operation.create(amount: 25, odate: "Wed, 25 Jan 2023 00:00:00.000000000 UTC +00:00", description: "Pancake", category_id: @category.id, user_id: @user1.id)
   end
 
   test "should get index" do
@@ -20,36 +22,39 @@ class OperationsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create operation" do
-  #   sign_in_as(@user1)
-  #   assert_difference -> { Operation.count }, 1 do
-  #     post operations_url, params: { operation: { amount: 25.00, odate: "25.01.2023", description: "cake", category_id: 1 } }
-  #   end
+  test "should create operation" do
+    sign_in_as(@user1)
+    assert_difference -> { Operation.count }, 1 do
+      post operations_url, params: { operation: { amount: 25.00, odate: "25.01.2023", description: "cake", category_id: @category.id } }
+    end
 
-  #   assert_redirected_to operation_path(Operation.last)
-  # end
+    assert_redirected_to operation_path(Operation.last)
+  end
 
-  # test "should show operation" do
-  #   sign_in_as(@user1)
-  #   get operation_url(@operation)
-  #   assert_response :success
-  # end
+  test "should show operation" do
+    sign_in_as(@user1)
+    get operation_url(@operation)
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get edit_operation_url(@operation)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_operation_url(@operation)
+    assert_response :success
+  end
 
-  # test "should update operation" do
-  #   patch operation_url(@operation), params: { operation: {  } }
-  #   assert_redirected_to operation_url(@operation)
-  # end
+  test "should update operation" do
+    # sign_in_as(@user)
+    patch operation_url(@operation), params: { operation: { amount: 25.00, odate: "25.01.2023", description: "cake", category_id: @category.id, user_id: @user1.id } }
+    assert_redirected_to operation_url(@operation)
+  end
 
-  # test "should destroy operation" do
-  #   assert_difference("Operation.count", -1) do
-  #     delete operation_url(@operation)
-  #   end
+  # how to check if not the same user try to delete or edit?
 
-  #   assert_redirected_to operations_url
-  # end
+  test "should destroy operation" do
+    assert_difference("Operation.count", -1) do
+      delete operation_url(@operation)
+    end
+
+    assert_redirected_to operations_url
+  end
 end
